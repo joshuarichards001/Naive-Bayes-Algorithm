3a19f68c35b66f5dfdbe3d22dc8ddf209648cd56
Josh Richards <joshuarichards001@gmail.com>
2020-06-08 11:20:10 +1200

    Solid refactor to simply the code down, very close to completion

--- src/naive_bayes_algorithm.py
+++ src/naive_bayes_algorithm.py
@@ -12,66 +12,53 @@ def file_parser(file_num):
         instance_value_list = line.split()
         temp = list()
         for value in instance_value_list:
-            temp.append(float(value))
+            temp.append(int(value))
         instances.append(temp)
     return instances
 
 
-# Split the dataset by class values, returns a dictionary
-def separate_by_class(dataset):
-    separated = dict()
-    for i in range(len(dataset)):
-        vector = dataset[i]
-        class_value = vector[-1]
-        if class_value not in separated:
-            separated[class_value] = list()
-        separated[class_value].append(vector)
-    return separated
+# Splits (into a dictionary) the training set given into the different classes that a instance can be
+def separate_train_by_class(train_set):
+    separated_data = dict()
+    for inst in train_set:
+        if inst[-1] not in separated_data:  # inst[-1] is the instances class
+            separated_data[inst[-1]] = list()
+        separated_data[inst[-1]].append(inst)
+    return separated_data
 
 
-# Calculate the mean, stdev and count for each column in a dataset
-def summarize_dataset(dataset):
-    summaries = [(statistics.mean(column), statistics.stdev(column), len(column)) for column in zip(*dataset)]
-    del (summaries[-1])
-    return summaries
+# Split train_set by class then calculates the stats for each row
+def info_by_class(train_set):
+    separated = separate_train_by_class(train_set)
+    col_info = dict()
+    for classification, rows in separated.items():
+        col_info[classification] = [(statistics.mean(column), statistics.stdev(column), len(column))
+                                    for column in zip(*rows)]  # get rows stats
+        del col_info[classification][-1]  # remove class stats
+    return col_info
 
 
-# Split dataset by class then calculate statistics for each row
-def summarize_by_class(dataset):
-    separated = separate_by_class(dataset)
-    summaries = dict()
-    for class_value, rows in separated.items():
-        summaries[class_value] = summarize_dataset(rows)
-    return summaries
-
-
-# Calculate the Gaussian probability distribution function for x
-def calculate_probability(x, mean, stdev):
-    exponent = math.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
-    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
-
-
-# Calculate the probabilities of predicting each class for a given row
-def calculate_class_probabilities(summaries, row):
-    total_rows = sum([summaries[label][0][2] for label in summaries])
+# Calculate the probability of each class for a given instance
+def calc_class_probabilities(class_model, inst):
+    total_rows = sum([class_model[classification][0][2] for classification in class_model])
     probabilities = dict()
-    for class_value, class_summaries in summaries.items():
-        probabilities[class_value] = summaries[class_value][0][2] / float(total_rows)
-        for i in range(len(class_summaries)):
-            mean, stdev, _ = class_summaries[i]
-            probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+    for classification, class_info in class_model.items():
+        probabilities[classification] = class_model[classification][0][2] / int(total_rows)
+        for i in range(len(class_info)):
+            class_prob = (1 / (math.sqrt(2 * math.pi) * class_info[i][1])) * \
+                   (math.exp(-((inst[i] - class_info[i][0]) ** 2 / (2 * class_info[i][1] ** 2))))
+            probabilities[classification] *= class_prob
     return probabilities
 
 
-# Predict the class for a given row
-def predict(summaries, row):
-    probabilities = calculate_class_probabilities(summaries, row)
-    best_label, best_prob = None, -1
-    for class_value, probability in probabilities.items():
-        if best_label is None or probability > best_prob:
-            best_prob = probability
-            best_label = class_value
-    return best_label
+# Returns the predicted class and its probability for a given instance
+def predict(class_model, inst):
+    probabilities = calc_class_probabilities(class_model, inst)
+    class_prediction = top_probability = -1
+    for classification, probability in probabilities.items():
+        if probability > top_probability:
+            class_prediction, top_probability = classification, probability
+    return class_prediction, top_probability
 
 
 # Main code
@@ -79,12 +66,10 @@ if __name__ == '__main__':
     training_set = file_parser(-2)  # First file
     test_set = file_parser(-1)  # Second file
 
-    # fit model
-    model = summarize_by_class(training_set)
-
-    for inst in test_set:
-        # predict the label
-        predicted_class = int(predict(model, inst))
-        print('Data=%s, Predicted: %s' % (inst, predicted_class))
+    model = info_by_class(training_set)
+    for instance in test_set:
+        predicted_class, class_probability = predict(model, instance)
+        print('Test Instance=%s, Predicted Class=%s, Probability=%s'
+              % (instance, predicted_class, '{0:f}'.format(class_probability)))
 
 
