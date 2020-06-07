a5e23e245902f6dfc147cfb64840e6a63368e089
Josh Richards <joshuarichards001@gmail.com>
2020-06-07 21:21:26 +1200

    First functional iteration, still requires a refactor

--- src/naive_bayes_algorithm.py
+++ src/naive_bayes_algorithm.py
@@ -1,11 +1,6 @@
+import math
 import sys
-
-
-# A instance within the data (represents a line in the data) has a class and a set of values
-class Instance:
-    def __init__(self, classification, values):
-        self.classification = classification
-        self.values = values
+import statistics
 
 
 # Parses a file and turns it into a list of instances
@@ -13,16 +8,83 @@ def file_parser(file_num):
     instances = list()
     with open(sys.argv[file_num]) as f:
         lines = [line.rstrip() for line in f]
-    for line in lines[1:]:
+    for line in lines:
         instance_value_list = line.split()
-        temp = Instance(instance_value_list[0], list())
-        for value in instance_value_list[1:]:
-            temp.values.append(value)
+        temp = list()
+        for value in instance_value_list:
+            temp.append(float(value))
         instances.append(temp)
     return instances
+
+
+# Split the dataset by class values, returns a dictionary
+def separate_by_class(dataset):
+    separated = dict()
+    for i in range(len(dataset)):
+        vector = dataset[i]
+        class_value = vector[-1]
+        if class_value not in separated:
+            separated[class_value] = list()
+        separated[class_value].append(vector)
+    return separated
+
+
+# Calculate the mean, stdev and count for each column in a dataset
+def summarize_dataset(dataset):
+    summaries = [(statistics.mean(column), statistics.stdev(column), len(column)) for column in zip(*dataset)]
+    del (summaries[-1])
+    return summaries
+
+
+# Split dataset by class then calculate statistics for each row
+def summarize_by_class(dataset):
+    separated = separate_by_class(dataset)
+    summaries = dict()
+    for class_value, rows in separated.items():
+        summaries[class_value] = summarize_dataset(rows)
+    return summaries
+
+
+# Calculate the Gaussian probability distribution function for x
+def calculate_probability(x, mean, stdev):
+    exponent = math.exp(-((x - mean) ** 2 / (2 * stdev ** 2)))
+    return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+
+
+# Calculate the probabilities of predicting each class for a given row
+def calculate_class_probabilities(summaries, row):
+    total_rows = sum([summaries[label][0][2] for label in summaries])
+    probabilities = dict()
+    for class_value, class_summaries in summaries.items():
+        probabilities[class_value] = summaries[class_value][0][2] / float(total_rows)
+        for i in range(len(class_summaries)):
+            mean, stdev, _ = class_summaries[i]
+            probabilities[class_value] *= calculate_probability(row[i], mean, stdev)
+    return probabilities
+
+
+# Predict the class for a given row
+def predict(summaries, row):
+    probabilities = calculate_class_probabilities(summaries, row)
+    best_label, best_prob = None, -1
+    for class_value, probability in probabilities.items():
+        if best_label is None or probability > best_prob:
+            best_prob = probability
+            best_label = class_value
+    return best_label
 
 
 # Main code
 if __name__ == '__main__':
     training_set = file_parser(-2)  # First file
     test_set = file_parser(-1)  # Second file
+
+    # fit model
+    model = summarize_by_class(training_set)
+
+    for inst in test_set:
+        # predict the label
+        predicted_class = int(predict(model, inst))
+        print('Data=%s, Predicted: %s' % (inst, predicted_class))
+
+
